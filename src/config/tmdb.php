b14799c0158dba659e8e5abf4d0ea01bcdd1b19a
Sudipto <mail@sudipto.net>
2025-08-28 09:00:41 +0530

    Update config

--- src/config/tmdb.php
+++ src/config/tmdb.php
@@ -1,20 +1,26 @@
 <?php
-/**
- * @package php-tmdb\laravel
- * @author Mark Redeman <markredeman@gmail.com>
- * @copyright (c) 2014, Mark Redeman
- */
+
 return [
     /*
      * Api key
      */
     'api_key' => '',
+
+    /*
+     * Enable Cache
+     */
     // 'cache' => true,
     'cache' => [
-        'defaultTtl' => null,
         'enabled' => true,
+        'defaultTtl' => null,
     ],
+    /**
+     * Client options
+     */
     'client' => [
+        /**
+         * Use https
+         */
         'secure' => true,
     ],
 ];
